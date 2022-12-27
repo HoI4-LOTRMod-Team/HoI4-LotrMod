df1ab2b315c7449e4e58cf1bc9c5e4f59747cfbb
Benjamin <Helliaca@users.noreply.github.com>
2022-12-27 16:43:17 +0100

    Traits, portraits and other

---
+++ gfx/leaders/_small_portrait_gen/convert_to_small.py
@@ -0,0 +1,38 @@
+import sys
+import glob
+import os
+from wand import image
+
+def apply_mask(img, mask):
+    img.alpha_channel = True
+
+    with image.Image(width=img.width, height=img.height, background=image.Color("transparent")) as alpha_image:
+        #alpha_image.composite_channel("alpha", mask, "copy_alpha")
+        img.composite_channel("alpha", mask, "copy_alpha")
+
+def convert_image(img_file, out_path):
+	frame = image.Image(filename='empty_frame.png')
+	mask = image.Image(filename='mask.png')
+	img = image.Image(filename=img_file)
+
+	# resize image to small
+	img.resize(width=mask.width, height=mask.height, filter='box', blur=1)
+
+	apply_mask(img, mask)
+	frame.composite(img)
+
+	with frame as op:
+	    op.compression = 'dxt5'
+	    op.save(filename=out_path)
+
+
+if(len(sys.argv) > 1):
+	old_path = "../"+sys.argv[1]
+	print("Processing path " + old_path)
+	for file in glob.glob(old_path+"/*.dds"):
+		print("File:" + file)
+		newpath = "../"+sys.argv[1]+"/small"
+		newfile = newpath + "/" + os.path.basename(file)
+		if not os.path.exists(newpath):
+		    os.makedirs(newpath)
+		convert_image(file, newfile)
---
+++ gfx/leaders/_small_portrait_gen/readme.txt
@@ -0,0 +1,7 @@
+This is a script that automatically converts leader portaits into
+"small", advisor portraits.
+
+Usage:
+python convert_to_small.py some_folder
+will convert all portraits in the gfx/leaders/some_folder folder
+and put them inside a gfx/leaders/some_folder/small
