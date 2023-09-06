0ba321e735710b4981ba2478251fa133fa54a96d
Benjamin Kahl <benjamin@kahl.fi>
2023-09-06 17:13:29 +0200

    ea triggers, effects and decisions

--- common/scripted_effects/_generate_ea_resolutions.py
+++ common/scripted_effects/_generate_ea_resolutions.py
@@ -1,84 +1,123 @@
-content_string = """
+effects_content_string = """
 
-### Resolution: $NAME$
 
-# Effect when resolution $NAME$ is passed
-ea_resolution_$NAME$_effect = {
 
+
+########################
+### Effects: $NAME$
+########################
+
+
+ea_$NAME$_passed_effect = {
 	if = {
 		limit = { has_idea = ea_resolution_$NAME$_idea_1 }
 		swap_ideas = { remove_idea = ea_resolution_$NAME$_idea_1 add_idea = ea_resolution_$NAME$_idea_2 }
+		set_global_flag = { flag = ea_resolution_$NAME$_passed value = 2 }
 	}
 	else_if = {
-		limit = { has_idea = ea_resolution_$NAME$_idea_2 }
-		swap_ideas = { remove_idea = ea_resolution_$NAME$_idea_2 add_idea = ea_resolution_$NAME$_idea_3 }
+		limit = { has_idea = ea_resolution_alliance_idea_2 }
+		swap_ideas = { remove_idea = ea_resolution_alliance_idea_2 add_idea = ea_resolution_alliance_idea_3 }
+		set_global_flag = { flag = ea_resolution_$NAME$_passed value = 3 }
 	}
 	else = {
-		add_ideas = ea_resolution_$NAME$_idea_1
+		add_ideas = ea_resolution_alliance_idea_1
+		set_global_flag = { flag = ea_resolution_$NAME$_passed value = 1 }
 	}
 }
 
-# Tooltip of the currently active $NAME$ resolution
-ea_resolution_$NAME$_current_tteffect = {
-
-	custom_effect_tooltip = ea_resolution_current_tt
-
+ea_$NAME$_add_to_new_member = {
 	if = {
-		limit = { has_idea = ea_resolution_$NAME$_idea_1 }
-		show_ideas_tooltip = ea_resolution_$NAME$_idea_1
-	}
-	else_if = {
-		limit = { has_idea = ea_resolution_$NAME$_idea_2 }
-		show_ideas_tooltip = ea_resolution_$NAME$_idea_2
-	}
-	else_if = {
-		limit = { has_idea = ea_resolution_$NAME$_idea_3 }
-		show_ideas_tooltip = ea_resolution_$NAME$_idea_3
-	}
-	else = {
-		custom_effect_tooltip = ea_resolution_none_tt
-	}
-}
-
-# Tooltip of the next tier $NAME$ resolution
-ea_resolution_$NAME$_next_tteffect = {
-
-	custom_effect_tooltip = ea_resolution_next_tt
-
-	if = {
-		limit = { has_idea = ea_resolution_$NAME$_idea_1 }
-		show_ideas_tooltip = ea_resolution_$NAME$_idea_2
-	}
-	else_if = {
-		limit = { has_idea = ea_resolution_$NAME$_idea_2 }
-		show_ideas_tooltip = ea_resolution_$NAME$_idea_3
-	}
-	else = {
-		show_ideas_tooltip = ea_resolution_$NAME$_idea_1
-	}
-}
-
-# Adds the current $NAME$ resolution to a country
-ea_resolution_$NAME$_add_to_new_member = {
-
-	random_country = {
-		limit = { has_country_flag = is_elven_alliance_member }
+		limit = { has_global_flag = ea_resolution_$NAME$_passed }
 
 		if = {
-			limit = { has_idea = ea_resolution_$NAME$_idea_1 }
-			ROOT = { add_ideas = ea_resolution_$NAME$_idea_1 }
+			limit = { has_global_flag = { flag = ea_resolution_$NAME$_passed value = 1 } }
+			add_ideas = ea_resolution_$NAME$_idea_1
 		}
 		else_if = {
-			limit = { has_idea = ea_resolution_$NAME$_idea_2 }
-			ROOT = { add_ideas = ea_resolution_$NAME$_idea_2 }
+			limit = { has_global_flag = { flag = ea_resolution_$NAME$_passed value = 2 } }
+			add_ideas = ea_resolution_$NAME$_idea_2
 		}
 		else_if = {
-			limit = { has_idea = ea_resolution_$NAME$_idea_3 }
-			ROOT = { add_ideas = ea_resolution_$NAME$_idea_3 }
+			limit = { has_global_flag = { flag = ea_resolution_$NAME$_passed value = 3 } }
+			add_ideas = ea_resolution_$NAME$_idea_3
 		}
 	}
 }
 
+ea_$NAME$_add_to_queue = {
+	add_to_array = { global.ea_proposers_queue = ROOT }
+	add_to_array = { global.ea_resolutions_queue = token:ea_resolution_$NAME$ }
+
+	custom_effect_tooltip = ea_propose_resolution_tt
+	effect_tooltip = { ea_resolution_$NAME$_effect = yes }
+}
+"""
+
+
+
+triggers_content_string = """
+
+
+
+
+########################
+### Triggers: $NAME$
+########################
+
+
+ea_$NAME$_unlocked = {
+    has_global_flag = ea_$NAME$_resolutions_unlocked
+}
+
+ea_$NAME$_can_be_passed = {
+	custom_trigger_tooltip = {
+		tooltip = ea_resolution_unlocked_tt
+		OR = {
+			has_global_flag = { flag = ea_resolution_$NAME$_passed value < 2 }
+			AND = {
+				has_global_flag = { flag = ea_resolution_$NAME$_passed value < 3 }
+				has_global_flag = ea_$NAME$_advanced_resolutions_unlocked
+			}
+		}
+	}
+	custom_trigger_tooltip = {
+		tooltip = ea_resolution_is_in_queue_tt		
+		NOT = { is_in_array = { global.ea_resolutions_queue = token:ea_resolution_$NAME$ } }
+	}
+	custom_trigger_tooltip = {
+		tooltip = ea_already_passing_resolution_tt		
+		NOT = { is_in_array = { global.ea_proposers_queue = ROOT } }
+	}
+}
+"""
+
+
+decisions_content_string = """
+
+	# Propose resolution: $NAME$
+	propose_ea_resolution_$NAME$ = {
+        icon = GFX_decision_generic_naval
+		cost = elven_alliance_proposal_cost?25
+
+        available = {
+            NOT = { has_global_flag = ea_assembly_in_recess }
+        }
+		visible = {
+			NOT = { has_global_flag = ea_vote_in_progress }
+			ea_industry_$NAME$ = yes
+			ea_$NAME$_can_be_passed = yes
+		}
+
+		ai_will_do = {
+			factor = 0
+		}
+
+		fire_only_once = no
+
+		complete_effect = {
+			propose_ea_resolution_$NAME$ = yes
+		}
+	}
 """
 
 
@@ -88,4 +127,10 @@ name_list = { "alliance", "industry", "expansion", "trade", "peace", "research",
 print("# This file was generated by _generate_ea_resolutions.py")
 
 for name in name_list:
-    print(content_string.replace("$NAME$", name))
+    print(effects_content_string.replace("$NAME$", name))
+
+for name in name_list:
+    print(triggers_content_string.replace("$NAME$", name))
+
+for name in name_list:
+    print(decisions_content_string.replace("$NAME$", name))
