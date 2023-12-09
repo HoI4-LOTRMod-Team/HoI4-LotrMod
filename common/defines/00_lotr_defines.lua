3aa38a0441e3494e6e5b4bf08403a6e5a1c6477c
blastradius14 <70386606+blastradius14@users.noreply.github.com>
2023-12-09 23:34:19 +0100

    Updating Generic Tree
    updating tinkering to correct bad generic tree problems

---
+++ common/defines/00_lotr_defines.lua
@@ -0,0 +1,22 @@
+NDefines.NGame.START_DATE = "3018.1.1.12"
+NDefines.NGame.END_DATE = "4018.1.1.1"
+NDefines.NGame.HANDS_OFF_START_TAG = "RHD"
+
+NDefines.NAI.HIGH_COMMAND_ADDED_WEIGHT_FACTOR = 0.5	-- Weight multiplier for high_command advisors over other chosen advisor or idea types
+NDefines.NAI.CHIEF_ADDED_WEIGHT_FACTOR = 1.0			-- Weight multiplier for chief roles over other advisor or idea types
+NDefines.NAI.ADVISOR_SCORE_CHEAPER_IS_BETTER_FACTOR = 0.5  -- When scoring advisors, this define scales how much the AI prefers cheaper advisors over more expensive ones. 0.0 means no effect, 0.15 means a cost difference of 100 PP modifies the score by 15 %.
+NDefines.NAI.DESIGN_COMPANY_SCORE_MULTIPLIER = 1.25              -- score multiplier for hiring a design company
+NDefines.NAI.ARMY_CHIEF_SCORE_MULTIPLIER = 0.75                  -- score multiplier for hiring an army chief
+NDefines.NAI.AIR_CHIEF_SCORE_MULTIPLIER = 1.0                   -- score multiplier for hiring an air chief
+NDefines.NAI.POLITICAL_ADVISOR_SCORE_MULTIPLIER = 1.25           -- score multiplier for hiring political advisors
+
+NDefines.NOperatives.AGENCY_CREATION_FACTORIES = 3					-- Number of factories used to create an intelligence agency
+
+
+NDefines.NCharacter.OFFICER_CORP_ADVISOR_ENTRIES_IN_MENU = { "high_command", "theorist", "army_chief", "cavalry_chief", "navy_chief" }
+NDefines.NCharacter.OFFICER_CORP_HIGH_COMMAND_SLOTS_IN_MENU = 2 --For Alert manager to count the number of High Command Slots in the UI
+NDefines.NCharacter.POLITICAL_ADVISOR_SLOTS_IN_MENU = 2 --For Alert manager to count the number of Political Advisor Slots in the UI
+NDefines.NCharacter.DEFAULT_PP_COST_FOR_MILITARY_ADVISOR = 150	-- When an advisor does not have cost assigned this is the default used	
+NDefines.NCharacter.SPECIALIST_ADVISOR_MIN_RANK = 3
+NDefines.NCharacter.EXPERT_ADVISOR_MIN_RANK = 5
+NDefines.NCharacter.GENIUS_ADVISOR_MIN_RANK = 7
---
+++ common/defines/00_lotr_graphics.lua
@@ -0,0 +1,17 @@
+
+NDefines.NGraphics.DRAW_REFRACTIONS_CUTOFF = 0
+NDefines.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {230, 320, 500}  -- At what camera distance the VP name text disappears.
+NDefines.NGraphics.UNITS_DISTANCE_CUTOFF = 150
+
+NDefines.NGraphics.SUN_HEIGHT  = 850 -- higher means softer shadows and more intense light
+NDefines.NGraphics.SUN_DIFFUSE_COLOR   	= {0.14, 0.3, 1.0}
+NDefines.NGraphics.MOON_INTENSITY 			= 1.0
+
+NDefines.NFrontend.FRONTEND_POS_X = 1558.0
+NDefines.NFrontend.FRONTEND_POS_Y = 1900.0
+NDefines.NFrontend.FRONTEND_POS_Z = 1100.0
+
+NDefines.NGraphics.TRAIT_LINE_ASSIGNED_COLOR = { 1.0, 1.0, 0.0 } -- Color for parent dependency lines when the parent is assigned.
+NDefines.NGraphics.TRAIT_LINE_NON_ASSIGNED_COLOR = { 0.6, 0.6, 0.6 } -- Color for parent dependency lines when the parent is not assigned assigned.
+NDefines.NGraphics.TRAIT_LINE_HIGHLIGHT_COLOR = { 1.0, 1.0, 1.0 } -- Color for parent dependency lines to the parents when hovering over a trait.
+NDefines.NGraphics.TRAIT_INVALID_FOR_ASSIGNMENT_COLOR = { 0.8, 0.3, 0.3 }
