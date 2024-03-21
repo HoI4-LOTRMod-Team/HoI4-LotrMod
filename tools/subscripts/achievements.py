e9d2804f2a4bf966aa5025d3e61cc818d0c51ddd
Benjamin <Helliaca@users.noreply.github.com>
2024-03-21 10:59:26 +0100

    icon changes

---
+++ tools/subscripts/achievements.py
@@ -0,0 +1,34 @@
+from wand.image import Image
+import os
+
+def process_files(directory):
+    # Define the suffixes
+    grey_suffix = "_grey"
+    not_eligible_suffix = "_not_eligible"
+    
+    # Load the crossout image
+    with Image(filename='crossout.png') as crossout_img:
+        # Iterate through all .dds files in the given directory
+        for filename in os.listdir(directory):
+            if filename.endswith(".dds") and not filename.endswith(grey_suffix + ".dds") and not filename.endswith(not_eligible_suffix + ".dds"):
+                # Construct the file path
+                file_path = os.path.join(directory, filename)
+                # Process the image
+                with Image(filename=file_path) as img:
+                    # Convert to black and white by setting saturation to 0
+                    img.modulate(saturation=0)
+                    # Save the grey image
+                    grey_filename = filename.rsplit('.', 1)[0] + grey_suffix + '.dds'
+                    grey_file_path = os.path.join(directory, grey_filename)
+                    img.save(filename=grey_file_path)
+                    
+                    # Add the crossout image on top
+                    img.composite(crossout_img, left=0, top=0)
+                    # Save the not eligible image
+                    not_eligible_filename = filename.rsplit('.', 1)[0] + not_eligible_suffix + '.dds'
+                    not_eligible_file_path = os.path.join(directory, not_eligible_filename)
+                    img.save(filename=not_eligible_file_path)
+
+if __name__ == "__main__":
+    directory = r'C:\Users\kahl\Documents\Paradox Interactive\Hearts of Iron IV\mod\lotr\gfx\achievements'
+    process_files(directory)
