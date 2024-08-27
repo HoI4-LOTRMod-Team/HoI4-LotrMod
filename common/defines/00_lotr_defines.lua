ace47e159ea319edfff4210d92eda7c1d66ba196
Benjamin <Helliaca@users.noreply.github.com>
2024-08-27 17:07:51 +0200

    deploy changes

--- common/defines/00_lotr_defines.lua
+++ common/defines/00_lotr_defines.lua
@@ -102,5 +102,5 @@ NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 5			-- Max width of support in d
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 1		-- Max height of support in division designer.
 
 -- Training divisions
-NDefines.NProduction.MIN_POSSIBLE_TRAINING_MANPOWER = 2500	-- How many deployment lines minimum can be training
+NDefines.NProduction.MIN_POSSIBLE_TRAINING_MANPOWER = 3000	-- How many deployment lines minimum can be training
 NDefines.NProduction.MIN_FIELD_TO_TRAINING_MANPOWER_RATIO = 0.5
