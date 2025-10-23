a3826b3d42dffa854ed022a15c7a7292fbf51fbb
Benjamin Kahl <benjamin.kahl@iosb.fraunhofer.de>
2025-10-23 17:07:09 +0200

    removed redundant file

--- tools/misc/focus_tree_editor/basic_example2.py
+++ tools/misc/focus_tree_editor/basic_example2.py
@@ -8,7 +8,6 @@ from pdx_parser import Parse_PObj, Parse_List, PObj, SaveListToFile, ParseListFr
 from Qt import QtCore, QtWidgets
 
 from nodes import focus_node
-from nodes import test_image_node
 from NodeGraphQt import (
     NodeGraph,
     NodesPaletteWidget,
@@ -63,7 +62,6 @@ def main():
     # registered example nodes.
     graph.register_nodes([
         focus_node.FocusNode,
-        test_image_node.ImageNode
     ])
 
     # show the node graph widget.
@@ -127,8 +125,8 @@ def main():
     #focus_1 = graph.create_node('nodes.basic.FocusNode')
     #focus_1.set_layout_direction(1)
 
-    node = graph.create_node('nodes.basic.ImageNode')
-    node.set_image(r'C:\Users\ben32801\Documents\Paradox Interactive\Hearts of Iron IV\mod\lotr\tools\subscripts\res\leader_frame.png')
+    #node = graph.create_node('nodes.basic.ImageNode')
+    #node.set_image(r'C:\Users\ben32801\Documents\Paradox Interactive\Hearts of Iron IV\mod\lotr\tools\subscripts\res\leader_frame.png')
 
     
 
