d9274a6cfd1d76bbe1c8e65e4db0363cf42d12fc
Benjamin <Helliaca@users.noreply.github.com>
2025-12-07 19:18:14 +0100

    mir stuff

--- tools/vscode_tasks/hoi4_tasks.py
+++ tools/vscode_tasks/hoi4_tasks.py
@@ -8,15 +8,15 @@ from pdx_parser import *
 # CONFIGURATION
 
 BASE_PATH = Path(__file__).parent.parent.parent.resolve()
-ideas_file = BASE_PATH / r'common\ideas\dolguldur.txt'
-country_leader_traits_file = BASE_PATH / r'common\country_leader\00_traits_lotr_dgu.txt'
-army_leader_traits_file = BASE_PATH / r'common\unit_leader\00_traits_lotr_dolguldur.txt'
-characters_file = BASE_PATH / r'common\characters\DGU.txt'
-history_file = BASE_PATH / r'history\countries\DGU - Dolguldur.txt'
-scripted_effects_file = BASE_PATH / r'common\scripted_effects\dolguldur_scripted_effects.txt'
-scripted_triggers_file = BASE_PATH / r'common\scripted_triggers\dolguldur_scripted_effects.txt'
-events_file = BASE_PATH / r'events\DolGuldur.txt'
-tag = "DGU"
+ideas_file = BASE_PATH / r'common\ideas\mirkwood.txt'
+country_leader_traits_file = BASE_PATH / r'common\country_leader\00_traits_lotr_mir.txt'
+army_leader_traits_file = BASE_PATH / r'common\unit_leader\00_traits_lotr_mir.txt'
+characters_file = BASE_PATH / r'common\characters\MIR.txt'
+history_file = BASE_PATH / r'history\countries\MIR - Mirkwood.txt'
+scripted_effects_file = BASE_PATH / r'common\scripted_effects\mirkwood_scripted_effects.txt'
+scripted_triggers_file = BASE_PATH / r'common\scripted_triggers\mirkwood_scripted_effects.txt'
+events_file = BASE_PATH / r'events\Mirkwood.txt'
+tag = "MIR"
 
 
 new_idea_template = """
