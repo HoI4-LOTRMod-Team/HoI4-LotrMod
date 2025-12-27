ac11803df7af3fc032303503e84517f6198e30cb
Benjamin <Helliaca@users.noreply.github.com>
2025-12-27 10:20:56 +0100

    new cfm system progress

--- tools/vscode_tasks/hoi4_tasks.py
+++ tools/vscode_tasks/hoi4_tasks.py
@@ -8,10 +8,10 @@ from pdx_parser import *
 # CONFIGURATION
 
 BASE_PATH = Path(__file__).parent.parent.parent.resolve()
-ideas_file = BASE_PATH / r'common\ideas\dolguldur.txt'
-country_leader_traits_file = BASE_PATH / r'common\country_leader\00_traits_lotr_dgu.txt'
-army_leader_traits_file = BASE_PATH / r'common\unit_leader\00_traits_lotr_dgu.txt'
-characters_file = BASE_PATH / r'common\characters\DGU.txt'
+ideas_file = BASE_PATH / r'common\ideas\mirkwood.txt'
+country_leader_traits_file = BASE_PATH / r'common\country_leader\00_traits_lotr_mir.txt'
+army_leader_traits_file = BASE_PATH / r'common\unit_leader\00_traits_lotr_mir.txt'
+characters_file = BASE_PATH / r'common\characters\MIR.txt'
 history_file = BASE_PATH / r'history\countries\MIR - Mirkwood.txt'
 scripted_effects_file = BASE_PATH / r'common\scripted_effects\mirkwood_scripted_effects.txt'
 scripted_triggers_file = BASE_PATH / r'common\scripted_triggers\mirkwood_scripted_effects.txt'
