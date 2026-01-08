810301005e98307df16609775d190be9eb1fd725
Benjamin Kahl <benjamin.kahl@iosb.fraunhofer.de>
2026-01-08 10:09:21 +0100

    DGU focus filters

--- tools/misc/focus_tree_editor/focus_node.py
+++ tools/misc/focus_tree_editor/focus_node.py
@@ -118,7 +118,11 @@ class FocusNode(BaseNode):
                     self.pObj.Get("cost").value = str(value)
 
                 elif name == "filters" and self.pObj.Has("search_filters"):
-                    self.pObj.Get("search_filters").value = "{" + " ".join(self.filters) + " }"
+                    super().__setattr__(name, value)
+                    if len(self.filters) < 1:
+                        self.pObj.Get("search_filters").value = "{ }"
+                    else:
+                        self.pObj.Get("search_filters").value = "{ " + " ".join(self.filters) + " }"
 
                 elif name == "relative_position_id":
                     if self.pObj.Has("relative_position_id"):
