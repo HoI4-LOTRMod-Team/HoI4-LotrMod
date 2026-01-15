5fb45b49368eb8debb07a71be18efd2aaeef54bd
Benjamin Kahl <benjamin.kahl@iosb.fraunhofer.de>
2026-01-15 11:07:51 +0100

    some limited RGL work

--- tools/misc/event_chain_editor/app_event_tree_editor.py
+++ tools/misc/event_chain_editor/app_event_tree_editor.py
@@ -110,8 +110,8 @@ def main():
     graph = create_node_graph()
 
     focus_node_tree = FocusNodeTree(graph,
-        BASE_PATH / r'events\Dolguldur.txt',
-        BASE_PATH / r'localisation\english\dolguldur\dolguldur_events_l_english.yml'
+        BASE_PATH / r'events\Lotr_Generic.txt',
+        BASE_PATH / r'localisation\english\0_lotr_core\lotr_generic_events_l_english.yml'
     )
     graph.focus_tree = focus_node_tree
 
