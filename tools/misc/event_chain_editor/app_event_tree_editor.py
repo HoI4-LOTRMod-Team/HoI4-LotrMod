b2285274ed13e797e5fe09d2ad85bdf5a2939db6
Benjamin Kahl <benjamin.kahl@iosb.fraunhofer.de>
2026-01-20 14:00:14 +0100

    VAL work

--- tools/misc/event_chain_editor/app_event_tree_editor.py
+++ tools/misc/event_chain_editor/app_event_tree_editor.py
@@ -110,8 +110,8 @@ def main():
     graph = create_node_graph()
 
     focus_node_tree = FocusNodeTree(graph,
-        BASE_PATH / r'events\Anduin.txt',
-        BASE_PATH / r'localisation\english\anduin\anduin_events_l_english.yml'
+        BASE_PATH / r'events\Lotr_Generic.txt',
+        BASE_PATH / r'localisation\english\0_lotr_core\lotr_generic_events_l_english.yml'
     )
     graph.focus_tree = focus_node_tree
 
