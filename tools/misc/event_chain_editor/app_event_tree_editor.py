a4fe665558688b3f68cf1cc4db55d9783ca48172
Benjamin Kahl <benjamin.kahl@iosb.fraunhofer.de>
2026-01-22 12:05:40 +0100

    GOB work

--- tools/misc/event_chain_editor/app_event_tree_editor.py
+++ tools/misc/event_chain_editor/app_event_tree_editor.py
@@ -110,8 +110,8 @@ def main():
     graph = create_node_graph()
 
     focus_node_tree = FocusNodeTree(graph,
-        BASE_PATH / r'events\Bonus_events.txt',
-        BASE_PATH / r'localisation\english\bonus_events_l_english.yml'
+        BASE_PATH / r'events\GoblinTown.txt',
+        BASE_PATH / r'localisation\english\goblintown\goblintown_events_l_english.yml'
     )
     graph.focus_tree = focus_node_tree
 
