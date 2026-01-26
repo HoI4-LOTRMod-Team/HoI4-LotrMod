ed19b2e285cf35033976b25e91f0048875e7041c
Benjamin Kahl <benjamin.kahl@iosb.fraunhofer.de>
2026-01-26 14:27:16 +0100

    MIR work

--- tools/misc/event_chain_editor/app_event_tree_editor.py
+++ tools/misc/event_chain_editor/app_event_tree_editor.py
@@ -110,8 +110,8 @@ def main():
     graph = create_node_graph()
 
     focus_node_tree = FocusNodeTree(graph,
-        BASE_PATH / r'events\GoblinTown.txt',
-        BASE_PATH / r'localisation\english\goblintown\goblintown_events_l_english.yml'
+        BASE_PATH / r'events\Mirkwood.txt',
+        BASE_PATH / r'localisation\english\mirkwood\mirkwood_events_l_english.yml'
         #BASE_PATH / r'events\Lotr_Generic.txt',
         #BASE_PATH / r'localisation\english\0_lotr_core\lotr_generic_events_l_english.yml'
     )
