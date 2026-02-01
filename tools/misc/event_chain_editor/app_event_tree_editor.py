b4e9a0c2d76461a9093ddf29048921281fdfc17d
Benjamin <Helliaca@users.noreply.github.com>
2026-02-01 21:14:06 +0100

    GUN expedition work

--- tools/misc/event_chain_editor/app_event_tree_editor.py
+++ tools/misc/event_chain_editor/app_event_tree_editor.py
@@ -110,8 +110,8 @@ def main():
     graph = create_node_graph()
 
     focus_node_tree = FocusNodeTree(graph,
-        BASE_PATH / r'events\Mirkwood.txt',
-        BASE_PATH / r'localisation\english\mirkwood\mirkwood_events_l_english.yml'
+        BASE_PATH / r'events\Gundabad.txt',
+        BASE_PATH / r'localisation\english\bonus_events_l_english.yml'
         #BASE_PATH / r'events\Lotr_Generic.txt',
         #BASE_PATH / r'localisation\english\0_lotr_core\lotr_generic_events_l_english.yml'
     )
