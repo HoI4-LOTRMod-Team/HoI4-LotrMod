237cbf0a7c5109b028dd783d959a60b6b703f295
Benjamin Kahl <benjamin.kahl@iosb.fraunhofer.de>
2026-02-05 16:24:08 +0100

    small rhun-related stuff

--- tools/misc/focus_tree_editor/app_focus_tree_editor.py
+++ tools/misc/focus_tree_editor/app_focus_tree_editor.py
@@ -55,7 +55,7 @@ def main():
     # create graph controller.
     graph = create_node_graph()
 
-    focus_node_tree = FocusNodeTree(graph, BASE_PATH / "common/national_focus/gundabad.txt")
+    focus_node_tree = FocusNodeTree(graph, BASE_PATH / "common/national_focus/rhun.txt")
     graph.focus_tree = focus_node_tree
 
     context_menu = graph.get_context_menu('graph')
