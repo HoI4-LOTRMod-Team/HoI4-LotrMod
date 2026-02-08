5c055d8913e8bc1d0875329b5fe4527a6013bb50
Benjamin <Helliaca@users.noreply.github.com>
2026-02-08 14:55:04 +0100

    Rhun CW work

---
+++ tools/misc/get_all_focuses_under_focus.py
@@ -0,0 +1,36 @@
+
+
+
+
+from pdx_parser import *
+
+
+focus_path = r'common\national_focus\rhun.txt'
+
+root_focus_name = "RHUN_break_free_from_mordor"
+
+focus_tree = ParseObjFromFile(focus_path)
+
+child_focuses = set()
+
+child_focuses.add(root_focus_name)
+
+prev_child_focuses_len = 0
+
+while len(child_focuses) > prev_child_focuses_len:
+
+    prev_child_focuses_len = len(child_focuses)
+
+    for focus in focus_tree.GetAll("focus").value:
+        if focus.GetVal("id") in child_focuses:
+            continue
+        elif focus.Has("prerequisite"):
+            for preq in focus.Get("prerequisite").value:
+                if preq.value in child_focuses:
+                    child_focuses.add(focus.GetVal("id"))
+                    break
+        
+for f in child_focuses:
+    print(f)
+
+    
