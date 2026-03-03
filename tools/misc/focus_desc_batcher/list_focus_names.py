cb49393b017a9993efc619d8b2b6943ec533876e
Benjamin Kahl <benjamin.kahl@iosb.fraunhofer.de>
2026-03-03 16:51:33 +0100

    progress

--- tools/misc/focus_desc_batcher/list_focus_names.py
+++ tools/misc/focus_desc_batcher/list_focus_names.py
@@ -1,11 +1,14 @@
 
 
 from pdx_parser import *
+from locfile import LocFile
 
 
+FOCUS_FILE = r'C:\Users\ben32801\Documents\Paradox Interactive\Hearts of Iron IV\mod\lotr\common\national_focus\gundabad.txt'
 
-FOCUS_FILE = r'C:\Users\ben32801\Documents\Paradox Interactive\Hearts of Iron IV\mod\lotr\common\national_focus\rhun.txt'
 
+loc_file = LocFile(r'C:\Users\ben32801\Documents\Paradox Interactive\Hearts of Iron IV\mod\lotr\localisation\english\gundabad\gundabad_focus_l_english.yml')
 
 for f in ParseListFromFile_asPObj(FOCUS_FILE).Get("focus_tree").GetAll("focus").value:
-    print('"' + f.Get("id").value + '",')
+#for f in ParseListFromFile_asPObj(FOCUS_FILE).GetAll("joint_focus").value:
+    print('("' + f.Get("id").value + '", ' + '"' + loc_file.get(f.Get("id").value) + '"),')
--- tools/misc/focus_desc_batcher/test_file_creator.py
+++ tools/misc/focus_desc_batcher/test_file_creator.py
@@ -13,7 +13,7 @@ request_template = {
                 {"text": "PROMPT"},
                 {
                     "file_data": {
-                        "file_uri": "https://generativelanguage.googleapis.com/v1beta/files/hzvw9d5edpo8", 
+                        "file_uri": "https://generativelanguage.googleapis.com/v1beta/files/vst4h6wcnrga", 
                         "mime_type": "text/plain"
                     }
                 },
@@ -28,7 +28,7 @@ request_template = {
 prompt_template = """
 I am working on a HoI4 mod that takes place in the LOTR universe. 
 
-I'm currently working on a focus tree for the Easterlings of Rhûn.
+I'm currently working on a focus tree for Gundabad, which can stay as orcs or go down a 'Gundabad Expedition' path, where either dwarves or men reclaim it.
 The implementation is complete and ready, but I need your help in writing some flavor-descriptions for the focuses.
 
 I've attached the entire file of the focus tree so that you can deduce the focus tree structure and the general paths and effects. Some parts are still work-in-progress.
@@ -50,6 +50,7 @@ elu_operationglimmerpeakassault_desc:0 "'Wait, what's that? Is that a dwar-- AUU
 As you can see, the descriptions are typically not longer than a few sentences and provide a flavorful description of each focuses' actions/effects, causes or context. On some occasions they may even stray into some mild humor or irony.
 
 I want you to provide me with 5 different potential descriptions only for the following focus: PROMPT
+Note that this focuses true name is actually 'TRUE_NAME' (PROMPT is the id).
 
 Provide me the descriptions in the following format:
 
@@ -63,55 +64,98 @@ Provide only this list of descriptions and no other text at all.
 """
 
 prompt_list = [
-"RGL_a_people_under_threat",
-"RGL_the_politics_of_survival",
-"RGL_seek_a_protector",
-"RGL_stand_alone",
-"RGL_seek_guarantees",
-"RGL_seek_an_alliance",
-"RGL_seek_vassalage",
-"RGL_the_shroud_of_dol_guldur",
-"RGL_protection_of_the_golden_wood",
-"RGL_the_elven_kings_shield",
-"RGL_the_gladden_chiefs",
-"RGL_emergency_defense",
-"RGL_the_emergency_fyrd",
-"RGL_galadhrim_marchwardens",
-"RGL_invite_radagast",
-"RGL_orcish_lieutenants",
-"RGL_shared_army_exercises",
-"RGL_let_the_wizard_lead",
-"RGL_stability_through_safety",
-"RGL_provoke_an_attack",
-"RGL_economic_improvements",
-"RGL_diplomatic_outreach",
-"RGL_defensive_paranoia",
-"RGL_fortification_focus",
-"RGL_armed_resistance_focus",
-"RGL_build_a_forge",
-"RGL_provide_tribute",
-"RGL_brace_for_war",
-"RGL_defensive_measures",
-"RGL_military_production",
-"RGL_expand_the_workshops",
-"RGL_vigil_of_the_woods",
-"RGL_expand_woodside_villages",
-"RGL_charcoal_production",
-"RGL_set_up_additional_lumberyards",
-"RGL_protect_administrative_centers",
-"RGL_refugee_levies",
-"RGL_conceal_production",
-"RGL_freedom_fighters",
-"RGL_the_great_wall",
-"RGL_convert_farming_equipment",
-"RGL_desperate_defense",
-"RGL_increase_taxes",
-"RGL_emergency_powers",
-"RGL_try_to_barter_with_the_spiders",
-"RGL_tend_to_the_woods",
-"RGL_study_mirkwood_fauna",
-"RGL_rhosgobel_rabbits",
-"RGL_grow_bolder",
+("GUN_the_fate_of_gundabad", "The Fate of Gundabad"),
+("GUN_army_effort", "Army Effort"),
+("GUN_equipment_effort", "Equipment Effort"),
+("GUN_cavalry_effort", "Cavalry Effort"),
+("GUN_doctrine_effort", "Doctrine Effort"),
+("GUN_infrastructure_investment", "Infrastructure Investment"),
+("GUN_more_production_research", "More production research"),
+("GUN_civilian_production", "Civilian Production"),
+("GUN_more_civilian_production", "More Civilian Production"),
+("GUN_military_production", "Military Production"),
+("GUN_more_military_production", "More Military Production"),
+("GUN_focus_on_civilian_industry", "Prioritize Civilian Industry"),
+("GUN_focus_on_military_industry", "Prioritize Military Industry"),
+("GUN_make_room_in_the_mountain", "Make room in the Mountain"),
+("GUN_settle_our_internal_economy", "Settle our Internal Economy"),
+("GUN_the_orcs_of_the_north", "The Orcs of the North"),
+("GUN_follow_the_strong", "Rule of Strength"),
+("GUN_restore_border_outposts", "Restore Border Outposts"),
+("GUN_reinvigorate_the_broken_realm", "Reinvigorate Broken Spirits"),
+("GUN_spread_gradual_propaganda", "Slow Burning Flame"),
+("GUN_hate_and_violence", "Rage and Contempt"),
+("GUN_strengthen_our_leadership", "Empower Army Leaders"),
+("GUN_the_reawakened_kingdom", "The Reawakened Kingdom"),
+("GUN_exert_the_wrath_of_the_orcs", "Exert the Wrath of the Orcs"),
+("GUN_declare_a_new_king_of_orcs", "Declare a new King of Orcs"),
+("GUN_legacy_of_azog", "The Legacy of Azog"),
+("GUN_we_need_not_follow_others", "We Break Before We Bend"),
+("GUN_the_strength_of_others", "Strength in Allies"),
+("GUN_follow_angmar", "Follow Angmar"),
+("GUN_bow_to_saruman", "Bow to the White Wizard"),
+("GUN_join_sauron", "Submit to the Dark Lord"),
+("GUN_mutiny", "Mutiny!"),
+("GUN_strength_in_numbers", "Strength in Numbers"),
+("GUN_no_more_boldogs", "No more Boldogs"),
+("GUN_the_orc_revolution_abroad", "Support Orc Insurrections"),
+("GUN_depose_the_old_government", "Depose the old government"),
+("GUN_join_mordors_revolution", "Join the Orcs of Mordor"),
+("GUN_invite_moria_orcs", "Invite the Orcs of Moria"),
+("GUN_wary_of_the_necromancer", "Be Wary of the Necromancer"),
+("GUN_he_must_be_defeated", "Resist Sauron's Will"),
+("GUN_sauron_will_fall", "Topple the Dark Lord"),
+("GUN_remember_the_battle", "Remember the Defeat"),
+("GUN_restart_the_forges", "Restart the Forges"),
+("GUN_distant_campaigns", "Distant Campaigns"),
+("GUN_victory_or_death", "Victory or Death"),
+("GUN_vengeance_on_erebor", "Attack the Lonely Mountain"),
+("GUN_target_thranduil", "Assault the Elven King's Halls"),
+("GUN_destroy_the_bardings", "Destroy the Bardings"),
+("GUN_the_gundabad_expedition", "The Gundabad Expedition"),
+("GUN_a_message_to_angmar", "Seek Angmar's Support"),
+("GUN_a_message_to_beorn", "Seek Beorning Support"),
+("GUN_a_message_to_erebor", "Secure Support from Erebor"),
+("GUN_locate_weakspots", "Locate Weakspots"),
+("GUN_spy_on_the_orcs", "Spy on the Orcs"),
+("GUN_infiltrate_the_orcs", "Infiltrate the Orcs"),
+("GUN_raid_orc_infrastructure", "Raid Orcish Supplylines"),
+("GUN_the_new_inhabitants", "Reclaim the Fortress"),
+("GUN_the_dwarves_of_gundabad", "The Dwarves of Gundabad"),
+("GUN_home_of_the_dwarves", "Home of the Dwarves"),
+("GUN_mine_for_mithril", "Mine for Mithril"),
+("GUN_reignite_the_old_forges", "Reignite the Old Forges"),
+("GUN_reopen_the_old_mines", "Reopen the Old Mines"),
+("GUN_the_gold_trade", "The Gold Trade"),
+("GUN_further_iron_mining", "Expand Iron Mining"),
+("GUN_join_erebor", "Join Erebor"),
+("GUN_befriend_durin", "Befriend Durin"),
+("GUN_invite_him_to_our_faction", "Join Causes with Ered Luin"),
+("GUN_the_men_of_gundabad", "The Men of Gundabad"),
+("GUN_human_lead_industry", "Mannish Industry"),
+("GUN_welcome_more_people", "Invite More Settlers"),
+("GUN_join_angmar", "Join Angmar"),
+("GUN_form_a_militia_battalion", "Form a Militia Battalion"),
+("GUN_the_victorious_expedition", "A Victorious Expedition"),
+("GUN_a_hardy_people", "A Hardy People"),
+("GUN_a_strong_new_government", "Inspirational Leadership"),
+("GUN_the_expeditions_knowledge", "The Expedition's Knowledge"),
+("GUN_hunt_down_the_orcs", "Hunt Down Orc Stragglers"),
+("GUN_restore_the_old_fortress", "Restore the Fortress"),
+("GUN_join_gondor", "Join Gondor"),
+("GUN_restore_gundabad_industry", "Restore Gundabad Industry"),
+("GUN_repay_our_supporters", "Repay Our Supporters"),
+("GUN_the_mountain_research_institute", "The Mountain Library"),
+("GUN_products_of_research", "Products of Research"),
+("GUN_farming_efforts", "Farming Efforts"),
+("GUN_leather_tanneries", "Leather Tanneries"),
+("GUN_build_a_new_army", "Build a new Army"),
+("GUN_get_rid_of_all_the_orcs", "Preemptive Strikes"),
+("GUN_train_mountaineers", "Train Mountaineers"),
+("GUN_dwarven_human_cooperation", "Dwarven-Mannish Cooperation"),
+("GUN_the_skies_above", "The Skies Above"),
+("GUN_a_friendship_with_the_eagles", "Friendship With the Eagles"),
+("GUN_trade_with_the_woodsmen", "Trade with the Woodmen"),
 ]
 
 requests_list = [
@@ -120,8 +164,8 @@ requests_list = [
 
 
 i = 1
-for prompt in prompt_list:
-    p = prompt_template.replace("PROMPT", prompt)
+for (prompt, tn) in prompt_list:
+    p = prompt_template.replace("PROMPT", prompt).replace("TRUE_NAME", tn)
 
     req = copy.deepcopy(request_template)
     req["key"] = req["key"].replace("NUM", str(i))
