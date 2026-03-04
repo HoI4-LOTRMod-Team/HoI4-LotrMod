557a5a2df1c9c0ea54ec9dae1d0b015cf65a3139
Benjamin Kahl <benjamin.kahl@iosb.fraunhofer.de>
2026-03-04 11:58:25 +0100

    random stuff

--- tools/misc/focus_desc_batcher/list_focus_names.py
+++ tools/misc/focus_desc_batcher/list_focus_names.py
@@ -4,10 +4,10 @@ from pdx_parser import *
 from locfile import LocFile
 
 
-FOCUS_FILE = r'C:\Users\ben32801\Documents\Paradox Interactive\Hearts of Iron IV\mod\lotr\common\national_focus\rhun.txt'
+FOCUS_FILE = r'C:\Users\ben32801\Documents\Paradox Interactive\Hearts of Iron IV\mod\lotr\common\national_focus\spiders.txt'
 
 
-loc_file = LocFile(r'C:\Users\ben32801\Documents\Paradox Interactive\Hearts of Iron IV\mod\lotr\localisation\english\rhun\rhun_focuses_l_english.yml')
+loc_file = LocFile(r'C:\Users\ben32801\Documents\Paradox Interactive\Hearts of Iron IV\mod\lotr\localisation\english\spiders\spiders_focuses_l_english.yml')
 
 for f in ParseListFromFile_asPObj(FOCUS_FILE).Get("focus_tree").GetAll("focus").value:
 #for f in ParseListFromFile_asPObj(FOCUS_FILE).GetAll("joint_focus").value:
