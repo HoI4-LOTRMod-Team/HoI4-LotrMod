a593df9fb5f1cdc9f2d8a06cfab1a3d0ca214898
Benjamin <Helliaca@users.noreply.github.com>
2026-03-14 15:09:18 +0100

    big batch of DGU icons

--- tools/misc/icon_batcher/render_psds.py
+++ tools/misc/icon_batcher/render_psds.py
@@ -4,7 +4,7 @@ from psd_tools import PSDImage
 # --- SETUP ---
 # Replace this string with the actual path to your folder.
 # The 'r' before the string handles Windows backslashes automatically.
-DIRECTORY_PATH = r"E:\PROJECTS\HOI4_LOTR\out_batch5\out_batch5" 
+DIRECTORY_PATH = r"C:\Users\Kahl\Downloads\out_batch7\out_batch7" 
 
 def batch_convert_psds(folder_path):
     target_dir = Path(folder_path)
