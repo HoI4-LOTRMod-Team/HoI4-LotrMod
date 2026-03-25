388ff6843366805d3c6fa81fa04c54dbc5ca01ef
Benjamin Kahl <benjamin.kahl@iosb.fraunhofer.de>
2026-03-25 16:48:00 +0100

    TODO work

--- tools/misc/event_chain_editor/app_event_tree_editor.py
+++ tools/misc/event_chain_editor/app_event_tree_editor.py
@@ -110,9 +110,10 @@ def main():
     graph = create_node_graph()
 
     focus_node_tree = FocusNodeTree(graph,
-        BASE_PATH / r'events\GoblinTown.txt',
+        BASE_PATH / r'events\ring_events.txt',
         #BASE_PATH / r'localisation\english\0_lotr_core\lotr_temp_l_english.yml'
-        BASE_PATH / r'localisation\english\goblintown\goblintown_events_l_english.yml'
+        BASE_PATH / r'localisation\english\0_lotr_core\lotr_ring_l_english.yml',
+        #BASE_PATH / r'localisation\english\mirkwood\mirkwood_events_l_english.yml'
         #BASE_PATH / r'events\MorMenace.txt',
         #BASE_PATH / r'localisation\english\mordor\morm_events_l_english.yml'
         #BASE_PATH / r'events\rhunexpandedevents.txt',
