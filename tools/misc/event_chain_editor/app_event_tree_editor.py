e1bc8628d86cebeb69f1c818375cda0269dd31f0
Benjamin Kahl <benjamin.kahl@iosb.fraunhofer.de>
2026-03-27 12:10:57 +0100

    preliminary work on GAL mirror

--- tools/misc/event_chain_editor/app_event_tree_editor.py
+++ tools/misc/event_chain_editor/app_event_tree_editor.py
@@ -110,9 +110,9 @@ def main():
     graph = create_node_graph()
 
     focus_node_tree = FocusNodeTree(graph,
-        BASE_PATH / r'events\Mirkwood.txt',
+        BASE_PATH / r'events\MirrorEvents.txt',
         #BASE_PATH / r'localisation\english\0_lotr_core\lotr_generic_events_l_english.yml'
-        BASE_PATH / r'localisation\english\mirkwood\mirkwood_events_l_english.yml'
+        BASE_PATH / r'localisation\english\lothlorien\mirror_events_l_english.yml'
         #BASE_PATH / r'events\MorMenace.txt',
         #BASE_PATH / r'localisation\english\mordor\morm_events_l_english.yml'
         #BASE_PATH / r'events\rhunexpandedevents.txt',
