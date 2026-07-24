58b4a8878962fd50c48025d69e170cb842a06b23
Benjamin Kahl <benjamin.kahl@iosb.fraunhofer.de>
2026-07-24 13:41:44 +0200

    some very rough beginnings to moria

--- tools/misc/focus_tree_editor/app_focus_tree_editor.py
+++ tools/misc/focus_tree_editor/app_focus_tree_editor.py
@@ -55,7 +55,7 @@ def main():
     # create graph controller.
     graph = create_node_graph()
 
-    focus_node_tree = FocusNodeTree(graph, BASE_PATH / "common/national_focus/isengard.txt")
+    focus_node_tree = FocusNodeTree(graph, BASE_PATH / "common/national_focus/moria_new.txt")
     graph.focus_tree = focus_node_tree
 
     context_menu = graph.get_context_menu('graph')
